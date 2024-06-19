121b6cf48581e93eef2f2b2762a976d22a97210f
luisluna2307 <laluna@espol.edu.ec>
2024-06-19 14:33:23 -0500

    update wed class 19/6

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Indicator from './components/Indicator';
+import Summary from './components/Summary';
+import BasicTable from './components/BasicTable';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Grid container spacing={5}>
+      <Grid xs={6} md={4} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={6} md={4} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={6} md={4} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={6} md={4} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={6} md={4} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={6} md={4} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>
+        <Summary></Summary>
+      </Grid>
+      <Grid xs={12} md={6} lg={9} >
+        <BasicTable />
+      </Grid>
+
+
+    </Grid>
+
   )
 }
 
